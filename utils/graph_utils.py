a183d9bbd66a3cba4cf6691d999a17ebe3645dcd
Lucas Madriles <lmadriles@gmail.com>
2025-01-26 17:31:33 -0300

    Implemented direction differentiation

--- utils/graph_utils.py
+++ utils/graph_utils.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import pandas as pd
 import geopandas as gpd
+from itertools import pairwise
 
 
 def nodes_to_edges(df):
@@ -52,6 +53,97 @@ def generate_graph(gdf):
 
     return G
 
+# %%
+# Origem e destino para caminho:
+def must_pass(G, points, return_route=True):
+    """Rota precisa passar por mais de um ponto específico.
+    
+    Args:
+        G (networkx.Graph): O grafo representando o mapa.
+        points (list): Lista de pontos que a rota deve passar.
+        return_route (bool): Se True, retorna a rota completa junto com a distância total.
+                            Se False, retorna apenas a distância total.
+    
+    Returns:
+        Se return_route for True, retorna uma tupla (total_dist, total_route).
+        Se return_route for False, retorna apenas total_dist.
+    """
+    total_dist = 0
+    total_route = []
+
+    for i in range(len(points)-1):
+        if return_route:
+            dist_mid, route_mid = nx.single_source_dijkstra(G, source=points[i], target=points[i+1], weight='weight')
+            total_route.extend(route_mid[:-1])
+        else:
+            dist_mid = nx.dijkstra_path_length(G, source=points[i], target=points[i+1], weight='weight')
+        total_dist += dist_mid
+
+    if return_route:
+        total_route.append(route_mid[-1])  # adiciona ultimo ponto na rota
+        return total_dist, total_route
+    else:
+        return total_dist
+
+
+
+
+# %%
+# Projeções:
+def get_entrepatios(train, rail, sentido=False):
+    main_list = train.split()
+    main_list_ep = list(pairwise(main_list))
+    rail_ep = list(pairwise(rail))
+
+    if sentido:
+        #print("Flag ativada: o sentido de rail vai ser levado em consideração.")
+        return [item for item in main_list_ep if item in rail_ep] # interseçao
+
+    else: # sentido is False
+        crescente = list(pairwise(rail))
+        decrescente = list(pairwise(rail[::-1]))
+
+        intersec_crescente = [item for item in main_list_ep if item in crescente]
+        intersec_decrescente = [item for item in main_list_ep if item in decrescente]
+
+        return intersec_crescente + intersec_decrescente # união das duas interseccoes
+    
+def find_edge(G, entrepatio, operator):
+    try:
+        if G.edges[entrepatio]['ferrovia']==operator:
+            return G.edges[entrepatio]['weight']
+        else:
+            return 0
+    except:
+        return 0
+
+def train_on_rail(G, train, rail, tu, operator, valor_retorno='tku', sentido=False):
+    """Esta função exige que as estações estejam sequenciadas e sem buracos. 
+    Uma forma de garantir é utilizar o must_pass ou não eliminar duplicatas."""
+    km = 0
+    entrepatios = get_entrepatios(train, rail, sentido)
+
+    for par in entrepatios:
+        if find_edge(G, par, operator) is None:
+            print('Aresta não encontrada: ' + par + ' de ' + operator)
+
+        km += find_edge(G, par, operator)
+    
+    if valor_retorno == "dist":
+        return km
+    elif valor_retorno == "tku":
+        return km * tu
+    elif valor_retorno == "bool":
+        return bool(km)
+    elif valor_retorno == "tu":
+        return bool(km)*tu
+    else:
+        raise ValueError("Opção inválida para 'valor_retorno'. Use 'dist', 'tku' ou 'bool' ou 'tu'.")
+
+
+
+# %%
+
 def must_pass_dist(G, points):
     """Rota precisa passar por mais de um ponto específico"""
     total_dist = 0
@@ -61,47 +153,6 @@ def must_pass_dist(G, points):
         total_dist += dist_mid
 
     return total_dist
-
-def must_pass(G, points):
-    """Rota precisa passar por mais de um ponto específico"""
-    total_dist = 0
-    total_route = []
-
-    for i in range(len(points)-1):
-        dist_mid, route_mid = nx.single_source_dijkstra(G, source=points[i], target=points[i+1], weight='weight')
-        total_dist += dist_mid
-        total_route += route_mid[:-1]
-
-    total_route += [route_mid[-1]] # adiciona ultimo ponto na rota
-
-    return total_dist, total_route
-
-# %% Remover essa célula depois de testar as funções:
-# TKU no trecho:
-def get_projection_old(main_string, path):
-    path_set = set(path)
-    return [x for x in main_string.split() if x in path_set]
-
-def find_edge_old(G, a, b, malha):
-    try:
-        if G.edges[(a,b)]['ferrovia']==malha:
-            return G.edges[(a,b)]['weight']
-        else:
-            return 0
-    except:
-        return 0
-
-def find_tku_old(G, main_string, path, tu, malha):
-    km = 0
-    stations = get_projection_old(main_string, path)
-    for i in range(len(stations)-1):
-        if find_edge_old(G,stations[i], stations[i+1], malha) is None:
-            print(stations[i], stations[i+1], malha)
-
-        km += find_edge_old(G,stations[i], stations[i+1], malha)
-
-    tku = km * tu
-    return tku
 
 # %%
 # modify the get_projection function to not find edge if the two stations are not connected:
@@ -122,7 +173,7 @@ def get_projection(main_string, path):
     return result
 
 
-def find_edge(G, a, b, malha):
+def find_edge_v2(G, a, b, malha):
     try:
         if G.edges[(a,b)]['ferrovia']==malha:
             return G.edges[(a,b)]['weight']
@@ -135,10 +186,10 @@ def find_tku(G, main_string, path, tu, malha):
     km = 0
     stations = get_projection(main_string, path)
     for i in range(len(stations)-1):
-        if find_edge(G,stations[i], stations[i+1], malha) is None:
+        if find_edge_v2(G,stations[i], stations[i+1], malha) is None:
             print(stations[i], stations[i+1], malha)
 
-        km += find_edge(G,stations[i], stations[i+1], malha)
+        km += find_edge_v2(G,stations[i], stations[i+1], malha)
 
     tku = km * tu
     return tku
@@ -148,10 +199,10 @@ def find_tu(G, main_string, path, tu, malha):
     found = 0
     stations = get_projection(main_string, path)
     for i in range(len(stations)-1):
-        if find_edge(G,stations[i], stations[i+1], malha) is None:
+        if find_edge_v2(G,stations[i], stations[i+1], malha) is None:
             print(stations[i], stations[i+1], malha)
 
-        found = bool(find_edge(G,stations[i], stations[i+1], malha))
+        found = bool(find_edge_v2(G,stations[i], stations[i+1], malha))
         if found:
             break
 
@@ -162,9 +213,9 @@ def find_dist(G, main_string, path, malha):
     km = 0
     stations = get_projection(main_string, path)
     for i in range(len(stations)-1):
-        if find_edge(G,stations[i], stations[i+1], malha) is None:
+        if find_edge_v2(G,stations[i], stations[i+1], malha) is None:
             print(stations[i], stations[i+1], malha)
 
-        km += find_edge(G,stations[i], stations[i+1], malha)
+        km += find_edge_v2(G,stations[i], stations[i+1], malha)
 
     return km
